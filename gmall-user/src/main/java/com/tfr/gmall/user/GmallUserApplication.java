08cc05d56f6da7188c97953330d0a998b63a0d14
TangFangrun <578909881@qq.com>
2020-03-31 19:53:25 +0800

    test

---
+++ gmall-user/src/main/java/com/tfr/gmall/user/GmallUserApplication.java
@@ -0,0 +1,17 @@
+package com.tfr.gmall.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.tfr.gmall.user.mapper")
+public class GmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserApplication.class, args);
+    }
+
+}
